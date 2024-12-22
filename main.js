8cb217478685a61bc31c8398abc2a7e292bcfa3a
nadun <nadunsumanarathna@gmail.com>
2024-12-23 03:09:05 +0530

    create additioanl details

---
+++ main.js
@@ -0,0 +1,13 @@
+function dataValidation() {
+  const customerNum = document.getElementById("customer-num").value;
+  const customerName = document.getElementById("customer-name").value;
+  const issueDate = document.getElementById("issue-date").value;
+  const perimeter = document.getElementById("perimeter").value;
+  const NumOfLines = document.getElementById("num_of_lines").value;
+  const NumOfCorners = document.getElementById("num_of_corners").value;
+  const NumOfGates = document.getElementById("num_of_gates").value;
+  const TypeOfFence = document.getElementById("type_of_fence").value;
+
+
+    alert(customerName +" "+ customerNum);
+}
